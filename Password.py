6d4d0247f58c75339b908896a79f8e1cc2af5788
unknown <bandiganchristian@gmail.com>
2026-04-14 18:41:12 +0800

    Added the password class to show encapsulation

---
+++ Password.py
@@ -0,0 +1,9 @@
+class Password:
+    def __init__(self,title,password):
+        self.title = title
+        self.__password = password   
+        pass
+    def get_password(self):
+        return self.__password
+    def set_password(self,new_pass):
+        self.__password = new_pass
