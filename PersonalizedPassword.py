2a7703bde7288cc2678b40539be599d20e9742cb
Cassious Rosha B Cataulin <qcrcataulin@tip.edu.ph>
2026-04-23 13:10:05 +0800

    Enhance password generation with nickname and favorite number

--- PersonalizedPassword.py
+++ PersonalizedPassword.py
@@ -3,7 +3,10 @@ from PasswordGenerator import PasswordGenerator
 
 class PersonalizedPassword(PasswordGenerator):
     def generate(self):
-        name = input("Enter your name: ")
+       name = input("Enter your name or nickname: ")
         year = input("Enter your birth year: ")
-        symbol = random.choice("!@#$%")
-        return name + year + symbol + random.choice("!@#$%") + random.choice("!@#$%")
+        favorite_number = input("Enter your favorite number: ")
+        symbol1 = random.choice("!@#$%-_*")
+        symbol2 = random.choice("!@#$%-_*")
+        symbol3 = random.choice("!@#$%-_*")
+        return name + year + symbol1 + favorite_number + symbol2 + symbol3
